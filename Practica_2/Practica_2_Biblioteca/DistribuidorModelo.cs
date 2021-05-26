d3b28fad01d47aecc28c545423fd02f50e6c691a
Jose Alanis <jose_alanis96@hotmail.com>
2021-05-26 17:47:49 -0600

    Se creó el archivo de clase para el modelo del distribuidor con sus propiedades.

---
+++ Practica_2/Practica_2_Biblioteca/DistribuidorModelo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2_Biblioteca
+{
+    public class DistribuidorModelo
+    {
+        public string IdDistribuidor { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Calle { get; set; }
+        public string NumeroCasa { get; set; }
+        public string Colonia { get; set; }
+
+    }
+}
