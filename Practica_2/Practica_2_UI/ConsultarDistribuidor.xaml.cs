0247d0c0ebefff1b657ed4c910f01108fac66d7d
Jose Alanis <jose_alanis96@hotmail.com>
2021-05-27 10:32:28 -0600

    Se agregó la ventana de consulta del distribuidor, su diseño y su logica.

---
+++ Practica_2/Practica_2_UI/ConsultarDistribuidor.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+using Practica_2_Biblioteca;
+
+namespace Practica_2_UI
+{
+    /// <summary>
+    /// Interaction logic for ConsultarDistribuidor.xaml
+    /// </summary>
+    public partial class ConsultarDistribuidor : Window
+    {
+        public ConsultarDistribuidor()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConexion sql = new SqlConexion();
+            try
+            {
+                DistribuidorModelo distribuidor = new DistribuidorModelo(DistribuidorIdText.Text);
+                DistribuidorDatagrid.ItemsSource = sql.ConsultarDistribuidor(distribuidor);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
