bf1d9022a940724822c7ad9e2f292c82310047f5
Jose Alanis <jose_alanis96@hotmail.com>
2021-05-26 19:11:00 -0600

    Se implementó el metodo de inserción de datos de distribuidor en la base de datos.

--- Practica_2/Practica_2_Biblioteca/SqlConexion.cs
+++ Practica_2/Practica_2_Biblioteca/SqlConexion.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace Practica_2_Biblioteca
 {
     public class SqlConexion
     {
+        public void InsertarDistribuidor(DistribuidorModelo distribuidor)
+        {
+            using (IDbConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["Practica#2"].ToString()))
+            {
+                var p = new DynamicParameters();
+                p.Add("@IdDistribuidor", distribuidor.IdDistribuidor);
+                p.Add("@FechaRegistro", distribuidor.FechaRegistro);
+                p.Add("@Nombres", distribuidor.Nombres);
+                p.Add("@ApellidoPaterno", distribuidor.ApellidoPaterno);
+                p.Add("@ApellidoMaterno", distribuidor.ApellidoMaterno);
+                p.Add("@Calle", distribuidor.Calle);
+                p.Add("@NumeroCasa", distribuidor.NumeroCasa);
+                p.Add("@Colonia", distribuidor.Colonia);
 
+                conexion.Execute("dbo.spDistribuidor_Insertar", p, commandType: CommandType.StoredProcedure);
+            }
+
+        }
     }
 }
