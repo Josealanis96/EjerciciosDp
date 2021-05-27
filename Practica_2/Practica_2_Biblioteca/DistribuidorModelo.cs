feb2a63cb91afa1359bf03c20444a2de14b648df
Jose Alanis <jose_alanis96@hotmail.com>
2021-05-26 19:12:25 -0600

    Se ingresó la propiedad faltante "Nombres" en la clase modelo de distribuidor y se implementó un metodo constructor.

--- Practica_2/Practica_2_Biblioteca/DistribuidorModelo.cs
+++ Practica_2/Practica_2_Biblioteca/DistribuidorModelo.cs
@@ -10,11 +10,23 @@ namespace Practica_2_Biblioteca
     {
         public string IdDistribuidor { get; set; }
         public DateTime FechaRegistro { get; set; }
+        public string Nombres { get; set; }
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
         public string Calle { get; set; }
-        public string NumeroCasa { get; set; }
+        public int NumeroCasa { get; set; }
         public string Colonia { get; set; }
 
+        public DistribuidorModelo(string idDistribuidor, DateTime fechaRegistro, string nombres, string apellidoPaterno, string apellidoMaterno, string calle, int numeroCasa, string colonia)
+        {
+            IdDistribuidor = idDistribuidor;
+            FechaRegistro = fechaRegistro;
+            Nombres = nombres;
+            ApellidoPaterno = apellidoPaterno;
+            ApellidoMaterno = apellidoMaterno;
+            Calle = calle;
+            NumeroCasa = numeroCasa;
+            Colonia = colonia;
+        }
     }
 }
